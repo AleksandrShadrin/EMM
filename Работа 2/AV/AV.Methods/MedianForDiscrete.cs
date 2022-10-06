95a23499c5d2c19543fccb3a22eefb813cc37bcd
Alex <ssaasshha11@yandex.ru>
2022-10-06 19:07:30 +0400

    Add evaluators for median

---
+++ Работа 2/AV/AV.Methods/MedianForDiscrete.cs
@@ -0,0 +1,32 @@
+﻿namespace AV.Methods
+{
+    public class MedianForDiscrete : IAverageDiscreteEvaluator
+    {
+        private readonly IEnumerable<double> _weights;
+
+        public MedianForDiscrete(IEnumerable<double> weights)
+        {
+            _weights = weights;
+        }
+
+        public double Calculate(IEnumerable<double> values)
+        {
+            var accumulatedFrequency = Enumerable
+                .Range(0, _weights.Count() - 1)
+                .Select(n =>
+                    _weights.Take(n + 1)
+                        .Aggregate(0d, (p, n) => p + n))
+                .ToList();
+
+            for (int i = 0; i < values.Count(); i++)
+            {
+                if (accumulatedFrequency[i] >= _weights.Sum() / 2)
+                {
+                    return values.ElementAt(i + 1);
+                }
+            }
+
+            throw new ArgumentException(nameof(values));
+        }
+    }
+}
---
+++ Работа 2/AV/AV.Methods/MedianForInterval.cs
@@ -0,0 +1,44 @@
+﻿using AV.Methods.ValueObjects;
+
+namespace AV.Methods
+{
+    public class MedianForInterval : IAverageIntervalEvaluator
+    {
+        private IEnumerable<double> _weights;
+
+        public MedianForInterval(IEnumerable<double> weights)
+        {
+            _weights = weights;
+        }
+
+
+
+        public double Calculate(IEnumerable<BaseInterval> intervals)
+        {
+            var accumulatedFrequency = Enumerable
+                .Range(0, _weights.Count() - 1)
+                .Select(n =>
+                    _weights.Take(n + 1)
+                        .Aggregate(0d, (p, n) => p + n))
+                .ToList();
+
+            var medianInterval = 0;
+
+            for (int i = 0; i < _weights.Count(); i++)
+            {
+                if (accumulatedFrequency[i] >= _weights.Sum() / 2)
+                {
+                    medianInterval = i;
+                    break;
+                }
+            }
+
+            var interval = (intervals.ElementAt(medianInterval) as ClosedInterval);
+
+            return interval.LeftBoundary.Value
+                + (interval.RightBoundary.Value - interval.LeftBoundary.Value)
+                * (0.5 * _weights.Sum() - accumulatedFrequency[medianInterval - 1])
+                / _weights.ElementAt(medianInterval);
+        }
+    }
+}
