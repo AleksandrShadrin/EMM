7ae207f35f6cde7b28e5c44e3cce90402284d87f
Alex <ssaasshha11@yandex.ru>
2022-10-06 16:49:46 +0400

    Add method to clone instance

--- Работа 2/AV/AV.Methods/ValueObjects/ClosedInterval.cs
+++ Работа 2/AV/AV.Methods/ValueObjects/ClosedInterval.cs
@@ -5,7 +5,7 @@
         public Boundary LeftBoundary { get; }
         public Boundary RightBoundary { get; }
 
-        public ClosedInterval(Boundary leftBoundary, Boundary rightBoundary)
+        public ClosedInterval(Boundary leftBoundary, Boundary rightBoundary) : base()
         {
             RightBoundary = rightBoundary is null ? throw new ArgumentNullException(nameof(rightBoundary)) : rightBoundary;
             LeftBoundary = leftBoundary is null ? throw new ArgumentNullException(nameof(leftBoundary)) : leftBoundary;
@@ -15,6 +15,11 @@
             return $"{(LeftBoundary.Include ? "[" : "(")}" +
                 $"{LeftBoundary};{RightBoundary}" +
                 $"{(RightBoundary.Include ? "]" : ")")}";
+        }
+
+        public ClosedInterval CloneWithNewBoundariesValues(double l, double r)
+        {
+            return new ClosedInterval(LeftBoundary with { Value = l }, RightBoundary with { Value = r });
         }
     }
 }
