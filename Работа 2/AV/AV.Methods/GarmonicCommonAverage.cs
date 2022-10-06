dbb53d426eb4c8b1844b9592f2e2bbd02fa2ff34
Alex <ssaasshha11@yandex.ru>
2022-10-06 17:39:10 +0400

    Add garmonic average avaluators

---
+++ Работа 2/AV/AV.Methods/GarmonicCommonAverage.cs
@@ -0,0 +1,8 @@
+﻿namespace AV.Methods
+{
+    public class GarmonicCommonAverage : IAverageDiscretEvaluator
+    {
+        public double Calculate(IEnumerable<double> values)
+            => values.Count() / values.Select(x => 1 / x).Sum();
+    }
+}
---
+++ Работа 2/AV/AV.Methods/GarmonicWeightedAverage.cs
@@ -0,0 +1,15 @@
+﻿namespace AV.Methods
+{
+    public class GarmonicWeightedAverage : IAverageDiscretEvaluator
+    {
+        private readonly IEnumerable<double> _weights;
+        public GarmonicWeightedAverage(IEnumerable<double> weights)
+        {
+            _weights = weights;
+        }
+
+        public double Calculate(IEnumerable<double> values)
+            => _weights.Sum() / Enumerable
+                .Zip(_weights, values, (w, v) => w / v).Sum();
+    }
+}
